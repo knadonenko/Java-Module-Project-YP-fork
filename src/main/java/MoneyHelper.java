80cc89d5285ba1884aae1dc1045f3d3dd636775f
Konstantin Nadonenko <kos891@gmail.com>
2024-06-04 19:58:15 +0300

    Фикс для хелпера по деньгам

--- src/main/java/MoneyHelper.java
+++ src/main/java/MoneyHelper.java
@@ -4,6 +4,11 @@ public class MoneyHelper {
 
         int intSum = (int) Math.floor(sum);
 
+        var preLastDigit = intSum % 100 / 10;
+        if (preLastDigit == 1) {
+            return "рублей";
+        }
+
         return switch (intSum % 10) {
             case 1 -> "рубль";
             case 2, 3, 4 -> "рубля";
